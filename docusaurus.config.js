55aefaeced39eb5d5e26b5812eb28d0fdbf7bd09
NiuBoss123 <i@niuboss123.com>
2022-08-11 00:09:20 +0800

    8.11第一次上传

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '千铃工艺 RingCraft',
+  tagline: 'Minecraft千铃工艺官方网站',
+  url: 'https://ring.bc413.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,15 +16,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'NiuBoss123', // Usually your GitHub org/user name.
+  projectName: 'Ring', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -33,6 +33,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -57,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: '千铃工艺 RingCraft',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -81,46 +82,42 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: '风采',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: '建筑',
+                to: '/',
               },
             ],
           },
           {
-            title: 'Community',
+            title: 'Links',
             items: [
               {
                 label: 'Stack Overflow',
                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
             ],
           },
           {
-            title: 'More',
+            title: '友情链接',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'Minecraft',
+                to: 'https://www.minecraft.net',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'NiuBoss123`s Home',
+                to: 'https://www.niuboss123.com',
+              },
+              {
+                label: 'BC413',
+                href: 'https://www.bc413.com',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} RingCraft, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
